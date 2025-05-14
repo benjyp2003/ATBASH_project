ac2c34cb42a91be1c60d35c67bdd16c2b1955ebc
Oryan Menachem <om3160204@gmail.com>
2025-05-14 17:45:25 +0300

    saved DangerPoints func once again

--- ATBASH_project/Program.cs
+++ ATBASH_project/Program.cs
@@ -5,27 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ATBASH_project
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(DangerPoints("bomb"));
-        }
-
-        static string DangerPoints(string decrypted)
-        {
-            int points = 0;
-            string[] arrayDecrypted = decrypted.Split(' ');
-            foreach (string word in arrayDecrypted)
-            {
-                if (word == "bomb" || word == "nukhba" || word == "fighter" || word == "rocket" || word == "secret")
-                    points++;
-            }
-            string strPoints = Convert.ToString(points);
-            return strPoints, decrypted;
-
-        }
-    
+        Console.WriteLine(DangerPoints("bomb"));
     }
+
+    static string DangerPoints(string decrypted)
+    {
+        int points = 0;
+        string[] arrayDecrypted = decrypted.Split(' ');
+        foreach (string word in arrayDecrypted)
+        {
+            if (word == "bomb" || word == "nukhba" || word == "fighter" || word == "rocket" || word == "secret")
+            { points++; }
+        }
+        string strPoints = Convert.ToString(points);
+        return strPoints, decrypted;
+
+    }
+
 }
